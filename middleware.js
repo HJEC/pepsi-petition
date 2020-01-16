bdfcb18ff393b037d80f9aad7d9c83fe01ab8950
HenCro <52827871+HJEC@users.noreply.github.com>
2020-01-16 13:40:40 +0100

    Middleware functions that check user session cookies and reroutes user

---
+++ middleware.js
@@ -0,0 +1,50 @@
+// middleware function to send user without userId token to register page
+exports.requireLoggedOutUser = function(req, res, next) {
+    if (req.session.userId) {
+        res.redirect("/petition");
+    } else {
+        next();
+    }
+};
+
+exports.noUserId = function(req, res, next) {
+    if (!req.session.userId) {
+        res.redirect("/login");
+    } else {
+        next();
+    }
+};
+
+// middleware function to send user without signature token to petition page to sign form.
+exports.requireSignature = function(req, res, next) {
+    if (!req.session.signatureId) {
+        res.redirect("/petition");
+    } else {
+        next();
+    }
+};
+
+//middleware function that allows users who do have signature token to see thank you page.
+exports.requireNoSignature = function(req, res, next) {
+    if (req.session.signatureId) {
+        res.redirect("/thanks");
+    } else {
+        next();
+    }
+};
+
+exports.requireProfileId = function(req, res, next) {
+    if (!req.session.profileId) {
+        res.redirect("/profile");
+    } else {
+        next();
+    }
+};
+
+exports.noProfileId = function(req, res, next) {
+    if (req.session.profileId) {
+        res.redirect("/petition");
+    } else {
+        next();
+    }
+};
